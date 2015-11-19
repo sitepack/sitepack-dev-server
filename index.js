6c765cda2cc7b34e6772385f5b80fc5444d15529
Jas Chen <jas228@gmail.com>
2015-11-19 14:20:24 +0800

    v0.4.0
    pass path to renderHtml()

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 var path = require('path');
 var url = require('url');
-
 var express = require('express');
 var webpack = require('webpack');
-
 var WebpackDevServer = require('webpack-dev-server');
 var proxy = require('proxy-middleware');
 
 var config = require(path.join(process.cwd(), 'webpack', 'config.dev.js'));
-var routes = require(path.join(process.cwd(), 'config', 'route.js'));
 var renderHtml = require(path.join(process.cwd(), 'base', 'html.js'));
-
 
 var assets = 'assets';
 var port = 8080;
@@ -18,19 +14,13 @@ var app = express();
 
 config.entry.main.unshift('webpack-dev-server/client?http://localhost:8081');
 config.output.publicPath = 'http://localhost:8081/assets/';
-
 app.use(`/${assets}`, proxy(url.parse('http://localhost:8081/assets')));
+
 app.use(express.static(path.join(process.cwd(), 'public')));
 
 app.get('/*', function response(req, res, next) {
   if (req.accepts('html') && req.originalUrl !== '/favicon.ico') {
     var title;
-
-    routes.forEach(function(route) {
-      if (route.path === req.originalUrl) {
-        title = route.title;
-      }
-    });
 
     var content = '';
     var cssHrefs = [];
@@ -40,7 +30,7 @@ app.get('/*', function response(req, res, next) {
     ];
 
     var html = renderHtml(
-      title,
+      req.originalUrl,
       content,
       cssHrefs,
       scriptSrcs
